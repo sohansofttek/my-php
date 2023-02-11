4db4e2eebe0b101fa574e521be6a694f6929df66
sohansofttek <1720123.sohanurrahman@gmail.com>
2023-02-12 02:49:01 +0600

    boolean

---
+++ boolean.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>boolean value</title>
+</head>
+<body>
+    <?php
+    $s = true;
+    $aa = false;
+
+    echo "$s : " . $s;
+    echo"<br>";
+    echo "$aa : ". $aa;
+    
+    
+    
+    
+    
+    ?>
+    
+</body>
+</html>
