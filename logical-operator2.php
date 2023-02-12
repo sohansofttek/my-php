ac4797690298a2789a36b70b3a6effa8bc3e7fbd
sohansofttek <1720123.sohanurrahman@gmail.com>
2023-02-12 14:32:43 +0600

    logical operators

---
+++ logical-operator2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>logical operator2</title>
+</head>
+<body>
+    <?php
+     $s = 2;
+     $a = "2";
+        //equal to : ==
+        //identical to : ===
+        //not identical to :!==
+        // or operator ||
+        // and operator &&
+        //=== means same type & same value are printed
+
+     if($s == $a && $s == 0 && $s < 0)
+     {
+         echo"yess that is";
+     }
+     else{
+         echo"no it is different value";
+     }
+     
+    
+    
+    
+    
+    ?>
+</body>
+</html>
