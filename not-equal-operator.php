cf529a31247c2abcd7538af00f1001196164c0c6
sohansofttek <1720123.sohanurrahman@gmail.com>
2023-02-12 14:17:21 +0600

    not equal operator

---
+++ not-equal-operator.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $s = 2;
+    $a = "2";
+    //=== means same type & same value are printed
+    if($s != $a)
+    {
+        echo"all the same value";
+    }
+    else{
+        echo"all the different value";
+    }
+    
+    
+    
+    
+    ?>
+    
+</body>
+</html>
