24c386dab047d546313dfd63a19cfdcac227c88b
sohansofttek <1720123.sohanurrahman@gmail.com>
2023-02-12 13:04:40 +0600

    if else statements

---
+++ if-else-statements.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>if-else-statements.php</title>
+</head>
+<body>
+    <?php
+    $s = 5;
+    $a = 9;
+    if($a<$s)
+    {
+        echo("A is greater");
+    }
+    else{
+        echo ("S is greater");
+    }
+    
+    
+    
+    
+    
+    ?>
+    
+</body>
+</html>
