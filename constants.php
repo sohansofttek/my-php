1cd499583e5423d6120928dfd43797b7bc859a6e
sohansofttek <1720123.sohanurrahman@gmail.com>
2023-02-12 12:46:19 +0600

    constants

---
+++ constants.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>constants</title>
+</head>
+<body>
+    <?php
+    
+    define ("SOHAN",1720123);
+    //constant means variable oposite .and it is pre define 
+    echo SOHAN;
+    
+    
+    
+    
+    
+    
+    
+    
+    ?>
+    
+</body>
+</html>
