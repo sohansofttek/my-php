53c4522b5d0158a7fea92427d5bc7779b6fe6824
sohansofttek <1720123.sohanurrahman@gmail.com>
2023-02-27 02:49:31 +0600

    math function

---
+++ math-function.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>math function</title>
+</head>
+<body>
+  power :  <?php echo pow(4,2); ?>
+<br/>
+sqrt : <?php echo sqrt(25); ?>
+<br/>
+sin : <?php echo sin(90); ?>
+<br/>
+rand : <?php echo rand(1,20); ?> 
+<br/>
+mod : <?php echo 10%3 ;  ?>
+      <br/>
+
+     increment : <?php
+      $s = 15;
+      $s++; 
+      echo $s;
+      
+      ?>
+    <br/>
+   decrement : <?php
+    $a =10;
+    $a--;
+    echo $a;
+    
+    
+    ?>
+    
+      
+</body>
+</html>
