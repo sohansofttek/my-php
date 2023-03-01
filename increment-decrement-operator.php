fc200224e052aa5df03c47fc0f3f41a5b010a6a7
sohansofttek <1720123.sohanurrahman@gmail.com>
2023-03-01 16:38:06 +0600

    increment decrement operator

---
+++ increment-decrement-operator.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>increment decrement operator</title>
+</head>
+<body>
+    <?php
+    $x = 5;
+    //increment
+    echo $x++;
+    echo "<br/>";
+    //{+ one hole 1increment }r {++ hole 2 increment}
+    echo ++$x;
+    echo "<br/>";
+
+
+    //  decrement
+        echo $x--;
+         echo "<br/>";
+         echo --$x;
+    
+    
+    ?>
+    
+</body>
+</html>
