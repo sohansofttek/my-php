ea5bef3b7af7fa8048c72026c8e9884d34d9f28a
sohansofttek <1720123.sohanurrahman@gmail.com>
2023-03-01 13:55:28 +0600

    assaingment operator

---
+++ assaingment-operators.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>assaingment operators</title>
+</head>
+<body>
+    <?php
+    $a = 30;
+     $a /= 6;
+    //$a += 5;
+    //$a -= 4;
+    //$a *=3;
+    //$a %= 4;
+    echo ($a);
+    echo "<br/>";
+    
+    
+    
+    
+    
+    ?>
+    
+</body>
+</html>
