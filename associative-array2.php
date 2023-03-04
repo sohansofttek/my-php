7250c4f766c60c415b514737a202450fea7e13e7
sohansofttek <1720123.sohanurrahman@gmail.com>
2023-03-04 15:24:35 +0600

    associative array

---
+++ associative-array2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>associative array</title>
+</head>
+    <?php
+    $cars = array("sohan"=>"170123","satu"=>"1720122","nirob"=>"1720124","tarek"=>"1720114","subash"=>"1720112");
+    foreach($cars as $index => $value){
+        echo "Index name = $index and Rall number = $value <br>";
+    }
+    
+    
+    
+    
+    
+    
+    ?>
+<body>
+    
+</body>
+</html>
