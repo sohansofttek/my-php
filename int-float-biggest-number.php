088e197cc3eb15e201be4f069d36778a07d4e88f
sohansofttek <1720123.sohanurrahman@gmail.com>
2023-03-18 14:02:06 +0600

    biggest number int\floate

---
+++ int-float-biggest-number.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>biggesr number print</title>
+</head>
+<body>
+    <?php
+    //integer
+    echo PHP_INT_MAX;
+    //FLOAT
+    echo "<br/>";
+    echo PHP_FLOAT_MAX;
+    echo "<br/>";
+    $x =  PHP_INT_MAX + 1;
+    var_dump($x);
+    echo "<br/>";
+    $s= (int)'2_000_000_000';
+    echo ($x);
+    echo "<br/>";
+    echo PHP_VERSION;
+
+
+
+    ?>
+    
+</body>
+</html>
